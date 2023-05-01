33b6d2b9d7ca7ca0471869c9119a89b167646f5f
Júlio Ernany <julioernany@gmail.com>
2023-05-01 15:31:07 -0400

    Implements exception handling

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,13 @@
 import entities.Inputs;
 
+import java.util.InputMismatchException;
+
 public class Program {
     public static void main(String[] args) {
 
-    Inputs inp = new Inputs();
-    inp.soma();
+        Inputs inp = new Inputs();
+
+        inp.soma();
+
     }
 }
--- src/entities/Inputs.java
+++ src/entities/Inputs.java
@@ -1,18 +1,29 @@
 package entities;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Inputs {
     public void soma() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("n1: ");
-        int n1 = sc.nextInt();
-        System.out.println("n2: ");
-        int n2 = sc.nextInt();
-        System.out.println("res: " + (n1+n2));
+        boolean continua = false;
 
+        do {
+            try {
+                System.out.println("n1, n2:");
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                System.out.println(n1 + n2);
+                continua = false;
+                sc.close();
+            } catch (InputMismatchException e) {
+                System.out.println("input error");
+                sc.nextLine();
+                continua = true;
+            }
+        }while(continua == true);
 
+        System.out.println("program end");
     }
-
 }
 
